3231b66e0207f857614e865569ab7d72b5a062fa
huoxj <2567499168@qq.com>
2024-01-19 16:36:13 +0800

    runz basic pages

---
+++ pages/myinfo/myinfo.js
@@ -0,0 +1,14 @@
+Page({
+	data: {
+	  avatarUrl: '/images/avatar.png',
+	  name: 'NJUer',
+	  department: '母猪产后护理系'
+	},
+	handleTap: function() {
+	  wx.showToast({
+		title: 'TODO: 设置',
+		icon: 'none'
+	  })
+	}
+  })
+  
---
+++ pages/search/search.js
@@ -0,0 +1,43 @@
+Page({
+	data: {
+	  inputValue: '',
+	  historyList: [],
+	  recommendList: ['114514', 'qaq']
+	},
+  
+	onLoad: function () {
+	  this.setData({
+		historyList: wx.getStorageSync('historyList') || []
+	  })
+	},
+  
+	onInput: function (e) {
+	  this.setData({
+		inputValue: e.detail.value
+	  })
+	},
+  
+	onSearch: function () {
+	  
+	  const value = this.data.inputValue
+	  if (!value) {
+		return
+	  }
+	  const historyList = this.data.historyList
+	  if (historyList.indexOf(value) === -1) {
+		historyList.unshift(value)
+		wx.setStorageSync('historyList', historyList)
+	  }
+	  wx.navigateTo({
+		url: '/pages/search-result/search-result?query=' + value
+	  })
+	},
+  
+	clearHistory: function () {
+	  wx.removeStorageSync('historyList')
+	  this.setData({
+		historyList: []
+	  })
+	}
+  })
+  
