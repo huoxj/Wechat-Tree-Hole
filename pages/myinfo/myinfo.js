51c8e897ca80f98d1b1f5b09e44ca4e54f2885a4
huoxj <2567499168@qq.com>
2024-01-20 21:49:51 +0800

    runz postpage

--- pages/myinfo/myinfo.js
+++ pages/myinfo/myinfo.js
@@ -4,6 +4,11 @@ Page({
 	  name: 'NJUer',
 	  department: '母猪产后护理系'
 	},
+	onLoad: function(){
+		wx.setNavigationBarTitle({
+		  title: '我的',
+		})
+	},
 	handleTap: function() {
 	  wx.showToast({
 		title: 'TODO: 设置',
